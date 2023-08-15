bd30d17d617a6ef10cccc2b933ac0f2659c06114
bronson <disnamedoe@gmail.com>
2023-08-14 18:20:55 -0700

    added index.js and product tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,51 +1,87 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+// Import necessary modules
+const express = require('express');
+const router = express.Router();
+const db = require('../../models');
 
-// The `/api/categories` endpoint
-
+// Get all tags
 router.get('/', (req, res) => {
-  Category.findAll({
-    include: [Product],
+  db.Tag.findAll({
+    include: [db.Product] // Include associated Product data
   })
-    .then((categories) => res.json(categories))
-    .catch((err) => res.status(500).json(err));
+  .then((tagData) => {
+    res.json(tagData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
+// Get a single tag by its ID
 router.get('/:id', (req, res) => {
-  Category.findOne({
-    where: {
-      id: req.params.id,
-    },
-    include: [Product],
+  db.Tag.findByPk(req.params.id, {
+    include: [db.Product] // Include associated Product data
   })
-    .then((category) => res.json(category))
-    .catch((err) => res.status(400).json(err));
+  .then((tagData) => {
+    if (!tagData) {
+      res.status(404).json({ message: 'Tag not found' });
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
+// Create a new tag
 router.post('/', (req, res) => {
-  Category.create(req.body)
-    .then((category) => res.status(200).json(category))
-    .catch((err) => res.status(400).json(err));
+  db.Tag.create(req.body)
+  .then((newTag) => {
+    res.json(newTag);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
+// Update a tag's name by its ID
 router.put('/:id', (req, res) => {
-  Category.update(req.body, {
-    where: {
-      id: req.params.id,
+  db.Tag.update(
+    {
+      tag_name: req.body.tag_name
     },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then((updatedTag) => {
+    res.json(updatedTag);
   })
-    .then((category) => res.status(200).json(category))
-    .catch((err) => res.status(400).json(err));
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
+// Delete a tag by its ID
 router.delete('/:id', (req, res) => {
-  Category.destroy({
+  db.Tag.destroy({
     where: {
-      id: req.params.id,
-    },
+      id: req.params.id
+    }
   })
-    .then((category) => res.status(200).json(category))
-    .catch((err) => res.status(400).json(err));
+  .then((deletedTag) => {
+    res.json(deletedTag);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
